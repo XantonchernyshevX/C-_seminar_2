a017e0a5cb1a1d9a4c1f9212b0551cc624445b44
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-15 15:51:08 +0300

    Task_01.1

---
+++ Task_01.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа с помощью методов
+
+System.Console.WriteLine("min");
+int min = ReadInt();
+
+System.Console.WriteLine("max");
+int max = ReadInt();
+
+int ReadInt()
+{return int.Parse(Console.ReadLine());}
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int number = GetRandomNumber(min, max);
+System.Console.WriteLine(number);
+
+if (GetFirstDigit(number) > GetSecondDigit(number))
+{
+    System.Console.WriteLine("first");
+}
+else
+{
+    System.Console.WriteLine("second");
+}
+
+
+
+int GetFirstDigit(int number){return number/10;}
+
+int GetSecondDigit(int number){return number % 10;}
