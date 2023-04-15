fafac59b869ea571c3f8a5e80d39bc113545228e
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-15 20:27:26 +0300

    Task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите прогармму, которая принимает на вход два числа и проверяет, является ли одно квадратом другого
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+if(a == Math.Pow(b, 2) || b == Math.Pow(a, 2))
+{
+    Console.WriteLine("Одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
